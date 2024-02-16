87eb4735934b644d474092164142ee669ec1ec09
Theo-lbd <theo.labadie@outlook.com>
2024-02-16 12:31:23 +0100

    changements

--- admin/public/js/adminReplies.js
+++ admin/public/js/adminReplies.js
@@ -1,11 +1,11 @@
 function submitReply(messageId) {
     const replyText = document.getElementById(`reply-text-${messageId}`).value;
 
-    fetch('index.php?action=submitReply', { // Assurez-vous que cette URL est la bonne pour votre application
+    fetch('index.php?action=submitReply', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json', // S'assurer que le client s'attend à du JSON
+            'Accept': 'application/json',
         },
         body: `message_id=${messageId}&reply=${encodeURIComponent(replyText)}`
     })
@@ -13,20 +13,24 @@ function submitReply(messageId) {
         if (!response.ok) {
             throw new Error('La réponse du réseau n\'était pas ok');
         }
-        return response.json(); // convertit la réponse en JSON
+        return response.json();
     })
     .then(data => {
         if (data.success) {
             // Ajoutez la réponse au DOM
             const repliesContainer = document.getElementById(`replies-${messageId}`);
+        const noReplyText = repliesContainer.querySelector('.no-reply');
+            if (noReplyText) {
+                noReplyText.textContent = 'Réponses :';
+            }
             const newReply = document.createElement('div');
+
             newReply.classList.add('reply');
     
             // Créez un élément <strong> pour "admin :"
             const adminPrefix = document.createElement('strong');
             adminPrefix.textContent = 'admin :';
     
-            // Créez un noeud texte pour le texte de la réponse
             const replyTextNode = document.createTextNode(` ${replyText}`);
     
             // Ajoutez le préfixe et le texte de la réponse au nouvel élément de réponse
@@ -35,10 +39,8 @@ function submitReply(messageId) {
     
             repliesContainer.appendChild(newReply);
     
-            // Effacer le champ de texte
             document.getElementById(`reply-text-${messageId}`).value = '';
         } else {
-            // Ici, vous pouvez gérer les réponses où success est false
             alert(data.message || 'Erreur lors de l\'envoi de la réponse.');
         }
     })
@@ -49,9 +51,11 @@ function submitReply(messageId) {
 }
 
 document.querySelectorAll('button[id^="reply-btn-"]').forEach(btn => {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function(event) {
+        event.preventDefault();
         const messageId = this.id.replace('reply-btn-', '');
         submitReply(messageId);
     });
 });
+
 
--- view/conditionUtilisation.php
+++ view/conditionUtilisation.php
@@ -6,27 +6,27 @@
 
         <section>
             <h2>Acceptation des conditions</h2>
-            <p>En utilisant [Nom de l'application], vous acceptez d'être lié par ces conditions d'utilisation. Si vous n'êtes pas d'accord, veuillez ne pas utiliser notre application.</p>
+            <p>En utilisant Recette APP, vous acceptez d'être lié par ces conditions d'utilisation. Si vous n'êtes pas d'accord, veuillez ne pas utiliser notre application.</p>
         </section>
 
         <section>
             <h2>Utilisation permise</h2>
-            <p>Vous vous engagez à utiliser [Nom de l'application] de manière responsable et légale. Toute utilisation abusive ou illégale est strictement interdite.</p>
+            <p>Vous vous engagez à utiliser Recette APP de manière responsable et légale. Toute utilisation abusive ou illégale est strictement interdite.</p>
         </section>
 
         <section>
             <h2>Propriété intellectuelle</h2>
-            <p>Les contenus de [Nom de l'application], y compris les textes, graphiques, logos et images, sont protégés par des droits de propriété intellectuelle et appartiennent à [Nom de l'entreprise] ou à ses concédants de licence.</p>
+            <p>Les contenus de Recette APP, y compris les textes, graphiques, logos et images, sont protégés par des droits de propriété intellectuelle et appartiennent à [Nom de l'entreprise] ou à ses concédants de licence.</p>
         </section>
 
         <section>
             <h2>Comptes utilisateurs</h2>
-            <p>Vous êtes responsable de la sécurité de votre compte et de votre mot de passe. [Nom de l'application] ne sera pas responsable des pertes résultant d'un accès non autorisé à votre compte.</p>
+            <p>Vous êtes responsable de la sécurité de votre compte et de votre mot de passe. Recette APP ne sera pas responsable des pertes résultant d'un accès non autorisé à votre compte.</p>
         </section>
 
         <section>
             <h2>Limitation de responsabilité</h2>
-            <p>[Nom de l'application] ne sera pas responsable des dommages indirects, accidentels, spéciaux ou consécutifs résultant de l'utilisation ou de l'impossibilité d'utiliser notre service.</p>
+            <p>Recette APP ne sera pas responsable des dommages indirects, accidentels, spéciaux ou consécutifs résultant de l'utilisation ou de l'impossibilité d'utiliser notre service.</p>
         </section>
 
         <section>
